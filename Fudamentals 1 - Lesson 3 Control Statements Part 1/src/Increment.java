ceacb32b403d1d685a0ef9ac5ecb233c0c471a82
Sumant Chahil <schahil31@gmail.com>
2017-06-04 00:19:56 -0700

    added a file showing the differences between prefix incrementing and postfix incrementing
    -in postfix the value adds 1 after the value is used
    -in prefix the value adds 1 before the value is used

---
+++ Fudamentals 1 - Lesson 3 Control Statements Part 1/src/Increment.java
@@ -0,0 +1,22 @@
+
+public class Increment {
+
+	public static void main(String[] args) {
+		
+		int c;
+		
+		c = 5;
+		System.out.println( c );
+		System.out.println(c++);
+		System.out.println( c );
+		
+		System.out.println();
+		
+		c = 5;
+		System.out.println( c );
+		System.out.println(++c);
+		System.out.println( c );
+		
+	}
+	
+}
