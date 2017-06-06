981d9edce06df68262ca706189160e4ac660d330
Sumant Chahil <schahil31@gmail.com>
2017-06-05 23:48:26 -0700

    Added the Fundamentals 1 lesson 5 folder
    There are two files added
    -Craps.java and CrapsTest.java
    -In Craps.java the application is designed
    -The application simulates a Casino game called Craps
    -The application will roll two dice sum the dice and then decide if the
    user wins or loses
    -The application introduces the enumeration data type
    -As well as showing how to use methods, static and final variables
    
    -In CrapsTest the Craps application is tested for functionality

---
+++ Fundamentals 1 - Lesson 5 Methods  A Deeper Look/src/CrapsTest.java
@@ -0,0 +1,12 @@
+
+public class CrapsTest {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		Craps firstGame = new Craps();
+		
+		firstGame.play();
+	}
+
+}
