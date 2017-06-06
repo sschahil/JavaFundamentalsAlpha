3c8d7d2f3bb9698b72f3dcd26ecf57ed3f50c132
Sumant Chahil <schahil31@gmail.com>
2017-06-06 12:08:23 -0700

    added the functionality of using multidimensional arrays
    -How to declare and initialize a multidimensional array
    -Jagged arrays
    -How to use a nested for loop to iterate through the multidimensional
    array

--- Fundamentals 1 - Lesson 6 Arrays/src/InitArray.java
+++ Fundamentals 1 - Lesson 6 Arrays/src/InitArray.java
@@ -3,14 +3,26 @@ public class InitArray {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		int array[] = {32, 27, 64, 18, 95, 14, 90, 70, 60, 37};
+		int array1[][] = {{1,2,3} , {4,5,6}};
+		int array2[][] = {{1,2} , {3}, {4,5,6}};
 		
-		System.out.printf("%s%8s\n", "Index", "Value");
+		System.out.println("Values in array1 by row are");
+		outputArray(array1);
 		
-		for(int counter = 0; counter < array.length; counter++)
+		System.out.println("\nValues in array2 by row are");
+		outputArray(array2);
+	}
+	
+	public static void outputArray(int array[][]) {
+		
+		for (int row = 0; row < array.length; row++)
 		{
-			System.out.printf("%5d%8d\n", counter, array[counter]);
+			for (int column = 0; column < array[row].length; column++)
+			{
+				System.out.printf("%d ", array[row][column]);
+			}
+			
+			System.out.println();
 		}
 	}
-
 }
