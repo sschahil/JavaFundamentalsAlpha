b4c4eca3d30ee0b3519b5b72ed480baebb8cb394
Sumant Chahil <schahil31@gmail.com>
2017-06-07 02:09:45 -0700

    added the new versions of BasePlusCommissionEmployee4.java and CommissionEmployee3.java, the test file is the same as before
    -This code is the optimized way to write a heiarchial program
    -Private variables should always stay private so that they are properly
    encapsulated

---
+++ Fundamentals II - Lesson 1 OOP Inheritance/src/BasePlusCommissionEmployeeTest4.java
@@ -0,0 +1,22 @@
+
+public class BasePlusCommissionEmployeeTest4 {
+
+public static void main(String[] args) {
+		
+		BasePlusCommissionEmployee4 employee = new BasePlusCommissionEmployee4(
+				"Bob", "Lewis", "333-33-3333", 5000, .04, 300);
+		
+		System.out.println("Employee information obtained by get methods: \n");
+		System.out.printf("%s %s\n", "First name is", employee.getFirstName());
+		System.out.printf("%s %s\n", "Last name is", employee.getLastName());
+		System.out.printf("%s %s\n", "Social Security Number is", employee.getSocialSecurityNumber());
+		System.out.printf("%s %s\n", "Gross Sales is", employee.getGrossSales());
+		System.out.printf("%s %s\n", "Commission Rate is", employee.getCommissionRate());
+		System.out.printf("%s %s\n", "Base Salary is", employee.getBaseSalary());
+		
+		employee.setBaseSalary(1000);
+		
+		System.out.printf("\n%s:\n\n%s\n", "Updated employee information obtained by toString", employee.toString());
+	}
+	
+}
