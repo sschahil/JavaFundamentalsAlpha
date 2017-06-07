a3f76c8a3a7f0b5b25bd78a8dae7dbf0787d0e65
Sumant Chahil <schahil31@gmail.com>
2017-06-07 11:26:55 -0700

    added the Employee.java file an abstract class
    -This class will be used has a super class to define the:
    -firstName, lastName, and socialSecurityNumber of each employee
    -The abstract method is used because for each employee there will be
    different earnings methods
    -By defining it in Employee we are able to make sure to add this method
    and are ensuring proper programming technique

---
+++ Fundamentals II - Lesson 2 OOP Polymorphism/src/Employee.java
@@ -0,0 +1,48 @@
+
+public abstract class Employee {
+
+	private String firstName;
+	private String lastName;
+	private String socialSecurityNumber;
+	
+	public Employee (String first, String last, String ssn) {
+		
+		firstName = first;
+		lastName = last;
+		socialSecurityNumber = ssn;
+	}
+	
+	public void setFirstName(String first) {
+		firstName = first;
+	}
+	
+	public String getFirstName() {
+		return firstName;
+	}
+	
+	public void setLastName(String last) {
+		lastName = last;
+	}
+	
+	public String getLastName() {
+		return lastName;
+	}
+	
+	public void setSocialSecurityNumber(String ssn) {
+		socialSecurityNumber = ssn;
+	}
+	
+	public String getSocialSecurityNumber() {
+		return socialSecurityNumber;
+	}
+	
+	public String toString() {
+		
+		return String.format("%s %s\nsocial security number: %s", 
+				getFirstName(), getLastName(), getSocialSecurityNumber() );
+	}
+	
+	//abstract method overridden by subclass
+	public abstract double earnings(); // no implementation here
+	
+}
