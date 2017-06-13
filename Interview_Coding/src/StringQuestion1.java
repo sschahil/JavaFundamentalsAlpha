1bf8cf4ddd181d26a5b726d699339915bcdc1027
Sumant Chahil <schahil31@gmail.com>
2017-06-12 23:58:38 -0700

    Added a String question for Interview Questions
    -This question asks to make a palindrome check program
    -This is version 1 of the answer

---
+++ Interview_Coding/src/StringQuestion1.java
@@ -0,0 +1,39 @@
+import java.util.*;
+
+public class StringQuestion1 {
+	
+	static Scanner input = new Scanner(System.in);
+
+	private static String name;
+
+	public static void main(String[] args) {
+		
+		System.out.println("Enter a word to check for it being a palindrome");
+		name = input.nextLine();
+		
+		palindrome(name);
+	}
+
+	public static void palindrome(String name1) {
+
+		int index = 0;
+		char[] charArray1 = new char[name1.length()];
+
+		for(int i = (name1.length() - 1); i >= 0; i--)
+		{
+			charArray1[index] = name1.charAt(i);
+			index++; 
+		}
+		
+		String name2 = new String(charArray1);
+		
+		System.out.println(name1);
+		System.out.println(name2);
+		
+		if(name1.equals(name2))
+			System.out.println("The string " + name + " is a palindrome.");
+		else
+			System.out.println("The string " + name + " isn't a palindrome.");
+	}
+
+}
