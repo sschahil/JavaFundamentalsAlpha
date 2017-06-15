f729e9ebac2b67f6a4bcedf24529f4a9a811cfb9
Sumant Chahil <schahil31@gmail.com>
2017-06-15 14:06:42 -0700

    Added the method longetstPalindrome()
    -This method will find and display all the palindromes in a string
    -Working on the portion where it will show the longest palindrome

--- Interview_Coding/src/StringQuestion4.java
+++ Interview_Coding/src/StringQuestion4.java
@@ -3,9 +3,44 @@
 
 public class StringQuestion4 {
 
+	private static String randomWord = "heilikahannahlkjilijloollksjdlkji";
+	
+	public static void longestPalindrome(String word) {
+		
+		String[] substring = new String[500];
+		
+		for(int i = 0; i < word.length(); i++)
+		{
+			for(int j = (i + 1); j < (word.length() - (i + 1)); j++ )
+			{
+				substring[i] = word.substring(i,j);
+				int index = 0;
+				char[] charArray = new char[substring[i].length()];
+				
+				for(int k = (substring[i].length() - 1); k >= 0; k--)
+				{
+					charArray[index] = substring[i].charAt(k);
+					index++; 
+				}
+				
+				String name2 = new String(charArray);
+				
+				
+				if(substring[i].equals(name2))
+					System.out.println("The string " + substring[i] + " is a palindrome.");
+			}
+		}
+		
+		for(int a = 0; a < substring.length; a++)
+		{
+			String longestPalindrome
+		}
+		
+	}
+	
 	public static void main(String[] args) {
 		
-		
+		longestPalindrome(randomWord);
 		
 	}
 	
