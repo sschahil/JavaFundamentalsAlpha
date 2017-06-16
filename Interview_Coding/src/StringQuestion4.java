121f3345ea0efd0ec759740f237faab5c397f27c
Sumant Chahil <schahil31@gmail.com>
2017-06-15 18:02:38 -0700

    First version of application
    -This application can take a string and return the longest palindrome
    -Will show the longest palindrome and show the character length

--- Interview_Coding/src/StringQuestion4.java
+++ Interview_Coding/src/StringQuestion4.java
@@ -1,41 +1,49 @@
 //Write a function to find out longest palindrome in a given string?
 
+import java.util.ArrayList;
 
 public class StringQuestion4 {
 
-	private static String randomWord = "heilikahannahlkjilijloollksjdlkji";
+	private static String randomWord = "heilikahannahlkjilijloollksjdlkjiaaaaj";
 	
 	public static void longestPalindrome(String word) {
 		
-		String[] substring = new String[500];
+		String substring;
+		ArrayList<String> strLongest = new ArrayList<String>();
 		
 		for(int i = 0; i < word.length(); i++)
 		{
 			for(int j = (i + 1); j < (word.length() - (i + 1)); j++ )
 			{
-				substring[i] = word.substring(i,j);
+				substring = word.substring(i,j);
 				int index = 0;
-				char[] charArray = new char[substring[i].length()];
+				char[] charArray = new char[substring.length()];
 				
-				for(int k = (substring[i].length() - 1); k >= 0; k--)
+				for(int k = (substring.length() - 1); k >= 0; k--)
 				{
-					charArray[index] = substring[i].charAt(k);
+					charArray[index] = substring.charAt(k);
 					index++; 
 				}
 				
 				String name2 = new String(charArray);
 				
 				
-				if(substring[i].equals(name2))
-					System.out.println("The string " + substring[i] + " is a palindrome.");
+				if(substring.equals(name2))
+				{
+					//System.out.println("The string " + substring + " is a palindrome.");
+					strLongest.add(substring);
+				}
 			}
 		}
 		
-		for(int a = 0; a < substring.length; a++)
+		String longestPali = strLongest.get(0);
+		for(int i = 1; i < strLongest.size() - 1; i++)
 		{
-			String longestPalindrome
+			if(longestPali.length() < strLongest.get(i).length())
+				longestPali = strLongest.get(i);
 		}
 		
+		System.out.println("The longest palindrome in this string is " + longestPali + " and it is " + longestPali.length() + " characters long.");
 	}
 	
 	public static void main(String[] args) {
